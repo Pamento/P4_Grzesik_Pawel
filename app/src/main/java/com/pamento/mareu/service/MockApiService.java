4717d44be71391bcfd335466d606bff9a5a57713
Pamento <pamento.dev@gmail.com>
2020-04-23 15:31:31 +0200

    Extend FilterService for Api 21, phase 1. Create outside class FilterMeetingBy.

--- app/src/main/java/com/pamento/mareu/service/MockApiService.java
+++ app/src/main/java/com/pamento/mareu/service/MockApiService.java
@@ -5,7 +5,9 @@ import android.os.Build;
 import androidx.annotation.RequiresApi;
 
 import com.pamento.mareu.model.Meeting;
+import com.pamento.mareu.utils.FilterMeetingBy;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,15 +51,36 @@ public class MockApiService implements ApiService {
     @Override
     public List<Meeting> getMeetingsForOneDay(String date) {
         if (mMeetingsByHall != null) {
+            mMeetingsByHall = FilterMeetingBy.byValue(mMeetingsByHall,date);
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                return mMeetingsByHall.stream()
+//                        .filter(Meeting -> date.equals(Meeting.getDate())).collect(Collectors.toList());
+//            } else {
+//                List<Meeting> result = new ArrayList<>();
+//                for (Meeting meeting : mMeetingsByHall) {
+//                    if (meeting.getDate().equals(date)) {
+//                        result.add(meeting);
+//                    }
+//                }
+//                mMeetingsByHall = result;
+//            }
+        } else {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                return mMeetingsByHall.stream()
+                mMeetingsByDate = mMeetings.stream()
                         .filter(Meeting -> date.equals(Meeting.getDate())).collect(Collectors.toList());
             }
+
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            mMeetingsByDate = mMeetings.stream()
-                    .filter(Meeting -> date.equals(Meeting.getDate())).collect(Collectors.toList());
-        }
+        //else {
+            List<Meeting> result = new ArrayList<>();
+            for (Meeting meeting : mMeetings) {
+                if (meeting.getDate().equals(date)) {
+                    result.add(meeting);
+                }
+            }
+            mMeetingsByDate = result;
+
+        //}
         return mMeetingsByDate;
     }
 
@@ -65,9 +88,33 @@ public class MockApiService implements ApiService {
     @Override
     public List<Meeting> getMeetingsForOneHall(String hallName) {
         if (mMeetingsByDate != null) {
-            return mMeetingsByDate.stream().filter(Meeting -> hallName.equals(Meeting.getHall())).collect(Collectors.toList());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                return mMeetingsByDate.stream().filter(Meeting -> hallName.equals(Meeting.getHall())).collect(Collectors.toList());
+
+            } else {
+
+                List<Meeting> result = new ArrayList<>();
+                for (Meeting meeting : mMeetingsByDate) {
+                    if (meeting.getHall().equals(hallName)) {
+                        result.add(meeting);
+                    }
+                }
+                mMeetingsByDate = result;
+            }
         } else {
-            mMeetingsByHall = mMeetings.stream().filter(Meeting -> hallName.equals(Meeting.getHall())).collect(Collectors.toList());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                mMeetingsByHall = mMeetings.stream().filter(Meeting -> hallName.equals(Meeting.getHall())).collect(Collectors.toList());
+
+            } else {
+
+                List<Meeting> result = new ArrayList<>();
+                for (Meeting meeting : mMeetings) {
+                    if (meeting.getHall().equals(hallName)) {
+                        result.add(meeting);
+                    }
+                }
+                mMeetingsByHall = result;
+            }
         }
         return mMeetingsByHall;
     }
--- app/src/main/java/com/pamento/mareu/utils/FilterMeetingBy.java
+++ app/src/main/java/com/pamento/mareu/utils/FilterMeetingBy.java
@@ -1,11 +1,28 @@
 package com.pamento.mareu.utils;
 
-import android.widget.Filter;
-import android.widget.Filterable;
+import android.os.Build;
 
-public class FilterMeetingBy implements Filterable {
-    @Override
-    public Filter getFilter() {
-        return null;
+import com.pamento.mareu.model.Meeting;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class FilterMeetingBy {
+    private static List<Meeting> result;
+
+    public static List<Meeting> byValue(List<Meeting> meetings,String value) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            result = meetings.stream()
+                    .filter(Meeting -> value.equals(
+                            value.contains("/") ? Meeting.getDate() : Meeting.getHall())).collect(Collectors.toList());
+        } else {
+
+            for (Meeting meeting : meetings) {
+                if (meeting.getDate().equals(value)) {
+                    result.add(meeting);
+                }
+            }
+        }
+        return result;
     }
 }
